6dc3440071514cde42bce3e591ae3e4f10e58beb
e-d-s <edstestoperations@gmail.com>
2026-04-23 19:15:36 +0530

    Update arja.js

--- blocks/arja/arja.js
+++ blocks/arja/arja.js
@@ -1,1 +1,20 @@
+//Marquee
 
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`arja-${cols.length}-cols`);
+
+  // setup text for marquee
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const div = col.querySelector('div');
+      if (div) {
+        const divWrapper = div.closest('div');
+        if (divWrapper && divWrapper.children.length === 1) {
+          // picture is only content in column
+          divWrapper.classList.add('arja-text-col');
+        }
+      }
+    });
+  });
+}
