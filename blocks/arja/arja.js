b57bc0c873aa715ae2fa6aa45d4c6b6b0c4b07c7
e-d-s <edstestoperations@gmail.com>
2026-04-23 19:25:33 +0530

    Update arja.js

--- blocks/arja/arja.js
+++ blocks/arja/arja.js
@@ -1,10 +1,7 @@
-//Marquee
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`arja-${cols.length}-cols`);
 
-  // setup text for marquee
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const dv = col.querySelector('div');
